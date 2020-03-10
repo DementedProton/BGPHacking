cfdfab1d1270b8677faf5c5e743d705eff8d9629
Antoine dEstalenx <antdestalenx@gmail.com>
2020-03-10 17:13:58 +0100

    Working BGP crafter with signatures

---
+++ BGP_crafter.py
@@ -0,0 +1,52 @@
+from scapy.all import *
+from scapy.contrib.bgp import *
+from scapy.layers.inet import IP
+import hashlib
+from TCP_MD5_parser import get_md5_salt_from_bytes, check_password
+from scapy.utils import wrpcap
+from scapy.compat import raw
+import dpkt
+from scapy.layers.l2 import Ether
+ 
+
+def sign_single_packet(packet, password):
+    #add an MD5 signature so that the length contains the signature
+    packet[TCP].options = [(19,b"\x00"*16)]
+    #get bytes
+    raw_ip_data = raw(packet[IP])
+    raw_tcp_data = raw(packet[TCP])
+    #get the salt from these bytes
+    salt = get_md5_salt_from_bytes(raw_ip_data, raw_tcp_data)
+    #computes the hash
+    if type(password)!="bytes":
+        password = password.encode("utf-8")
+    salt += password
+    packet_hash = hashlib.md5(salt).digest()
+    #add the signature in the options
+    packet[TCP].options = [(19,packet_hash)]
+
+    return packet
+
+
+
+src_ipv4_addr = '192.168.12.1'  # eth0
+dst_ipv4_addr = '192.168.12.2'
+established_port = 36376
+expected_seq_num=1000 # ack
+current_seq_num=1500 # seq
+NLRI_PREFIX = '10.110.99.0/24'
+
+base = IP(src=src_ipv4_addr, dst=dst_ipv4_addr, proto=6, ttl=255)  
+tcp = TCP(sport=established_port, dport=179, seq=current_seq_num, ack=expected_seq_num, flags='PA')
+up = BGPUpdate(path_attr=[BGPPathAttr(type_flags=64, type_code=5, attribute=BGPPALocalPref(local_pref=100))], nlri=BGPNLRI_IPv4(prefix=NLRI_PREFIX))      
+hdr = BGPHeader(type=2, marker=0xffffffffffffffffffffffffffffffff)  
+# proto=6 represents that, TCP will be travelling above this layer. This is simple IPV4 communication.
+# dport=179 means, we are communicating with bgp port of the destination router/ host. sport is a random port over which tcp is established. seq and ack are the sequence number and acknowledgement numbers. flags = PA are the PUSH and ACK flags
+# update packet consist of path attributes and NLRI (Network layer reachability information),  type_code in path attributes is for which type of path attribute it is. [more][3]
+# type=2 means UPDATE packet will be the BGP Payload, marker field is for authentication. max hex int (all f) are used for no auth.
+
+packet = Ether()/ base / tcp / hdr / up
+
+packet_list = [packet]
+signed_packet = sign_single_packet(packet,"nana")
+wrpcap('bgp_crafted.pcap', signed_packet, append=False)
--- TCP_MD5_parser.py
+++ TCP_MD5_parser.py
@@ -1,64 +1,116 @@
 import sys, os
 import dpkt
 import hashlib
+import time
 
-def parse_packets_md5(file):
-    """ 
-        parse the pcap file looking for TCP MD5 signatures. 
-        This function is mostly inspired by the tool pcap2john: https://github.com/truongkma/ctf-tools/blob/master/John/run/pcap2john.py
+
+def get_md5_salt_from_bytes(raw_ip_data, raw_tcp_data):
+    """Parse the packet bytes to get the signature
+    
+    Args:
+        raw_ip_data (bytes): bytes of the IP packet
+        raw_tcp_data (bytes): bytes of the TCP packet
+    Returns:
+        bytes: salt for the TCP MD5 signature
     """
-    hashes = []                      
+    # TCP pseudo-header + TCP header + TCP segment data
+    # add TCP pseudo-header
+    tcp = dpkt.tcp.TCP()
+    tcp.unpack(raw_tcp_data)
+    length = len(raw_tcp_data)
+    header_length = tcp.off * 4
+    data_length = length - header_length
+    salt = raw_ip_data[12:12 + 8]  # src. and dest. IP
+    salt = salt + b"\x00"  # zero padding
+    salt = salt + raw_ip_data[9].to_bytes(1,"little")  # protocol
+    salt = salt + (length // 256).to_bytes(1,"little")  # segment length
+    salt = salt + (length % 256).to_bytes(1,"little")  # segment length
+    # add TCP header
+    salt = salt + raw_tcp_data[:16]  # TCP header without checksum
+    salt = salt + (b"\x00" * 4)  # add zero checksum
+    # add segment data
+    salt = salt + raw_tcp_data[header_length:header_length + data_length]
+    return salt
+
+def get_md5_salt(buf):
+    """Gets the TCP pseudo-header + TCP header + TCP segment data of a TCP packet. 
+    This form the salt of the MD5 signature of the packet
+    This function gets bytes from the dpkt packet and calls get_md5_salt_from_bytes()
+    
+    Args:
+        buf (dpkt buffer): The packet gotten from a dpkt reader object. The packet must have an Ethernet, IP and TCP layer
+    
+    Returns:
+        (bytes): The salt of the MD5
+    """
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    raw_ip_data = ip.pack()
+    raw_tcp_data = tcp.pack()
+    salt = get_md5_salt_from_bytes(raw_ip_data, raw_tcp_data)
+    return salt
+
+
+def get_md5_signature(buf):
+    """Parses a packet to find a TCP MD5 signature.
+    
+    Args:
+        buf (bytes): The packet as bytes. The packet must have an Ethernet, IP and TCP layer
+    
+    Returns:
+        (bytes): The signature of the MD5. If no signatures were found, returns None
+    """
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    md5_found = False
+    for opt_type, opt_data in dpkt.tcp.parse_opts(tcp.opts):
+        # skip over "undesired" option fields
+        # TCP_OPT_MD5 = 19 implies TCP MD5 signature, RFC 2385
+        if opt_type == 19:
+            md5_found = True
+            return opt_data
+    if md5_found == False:
+        # If no MD5 found return None
+        print("No MD5 signature found in the packet")
+        return None
+
+
+def check_if_TCP(buf):
+    eth = dpkt.ethernet.Ethernet(buf)
+    if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+        #if it is not an IP packet, skip it
+        return False
+    ip = eth.data
+    if ip.v != 4 or ip.p != dpkt.ip.IP_PROTO_TCP:
+        #if it is not an IPv4 TCP packet
+        return False
+    return True
+
+def parse_signed_packets_md5(file):
+    """Parse a pcap file, looking for TCP MD5 signed packets and returning a list of tuples (hash, salt)
+    
+    Args:
+        file (string): A pcap file name
+    
+    Returns:
+        list: A list of tuples (hash, salt)
+    """
+    # This function is mostly inspired by the tool pcap2john: https://github.com/truongkma/ctf-tools/blob/master/John/run/pcap2john.py
+    hashes = []         
     with open(input_file,'rb') as f:
         capture = dpkt.pcap.Reader(f)
         for _, buf in capture:
-            eth = dpkt.ethernet.Ethernet(buf)
-            if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-                #if it is not an IP packet, skip it
-                continue
-            ip = eth.data
-            if ip.v != 4 or ip.p != dpkt.ip.IP_PROTO_TCP:
-                #if it is not an IPv4 TCP packet
-                continue
-            tcp = ip.data
-            if tcp.off * 4 >= 40 and len(tcp.opts) > 18:
-                # if the TCP packet has an option which is more than 18 bytes long
-                raw_ip_data = ip.pack()
-                raw_tcp_data = tcp.pack()
-                length = len(raw_tcp_data)
-                for opt_type, opt_data in dpkt.tcp.parse_opts(tcp.opts):
-                    # skip over "undesired" option fields
-                    # TCP_OPT_MD5 = 19 implies TCP MD5 signature, RFC 2385
-                    if opt_type == 19:
-                        found = True
-                        break
-                if found and opt_type == 19 and len(opt_data) == 16:
-                    #if the MD5 option has been found, the other conditions are additional checks
-                    #opt_data contains the option
-                    header_length = tcp.off * 4
-                    data_length = length - header_length
-                    # print length, header_length, data_length
-                    # TCP pseudo-header + TCP header + TCP segment data
-                    # salt_length = 12 + 20 + data_length
-                    # add TCP pseudo-header
-                    salt = raw_ip_data[12:12 + 8]  # src. and dest. IP
-                    salt = salt + b"\x00"  # zero padding
-                    salt = salt + raw_ip_data[9].to_bytes(1,"little")  # protocol
-                    salt = salt + (length // 256).to_bytes(1,"little")  # segment length
-                    salt = salt + (length % 256).to_bytes(1,"little")  # segment length
-                    # add TCP header
-                    salt = salt + raw_tcp_data[:16]  # TCP header without checksum
-                    salt = salt + (b"\x00" * 4)  # add zero checksum
-                    # add segment data
-                    salt = salt + raw_tcp_data[header_length:header_length + data_length]
-                    hashes.append((opt_data, salt))
+            if check_if_TCP(buf):
+                packet_md5 = get_md5_signature(buf)
+                salt = get_md5_salt(buf)
+                hashes.append((packet_md5, salt))
     return hashes
 
-def write_hashes_to_file(hashes,output_file):
-    with open(output_file,'w') as out:
-        for h,salt in hashes:
-            out.write("{}:{}\n".format(h.hex(), salt.hex()))
-
 def check_password(packet_hash, salt, password):
+    if packet_hash is None:
+        packet_hash = ""
     print("{:<16} | {:<32} | {:<32} | {:<64}".format("Password","Packet Hash", "Computed Hash", "Salt (first 64 bytes)"))
     if type(password)!="bytes":
         password = password.encode("utf-8")
@@ -67,16 +119,21 @@ def check_password(packet_hash, salt, password):
     sys.stdout.write("{:<16} | {} | {} | {}\n".format(password.decode("utf-8"),packet_hash.hex(), h, salt.hex()[:64])) 
 
 def launch_hashcat(h,s,mask):
+    """Launch hashcat to launch a mask attack using the givan mask on a MD5 hash with the given salt
+    
+    Args:
+        h (bytes): the hash in bytes
+        s (bytes): the salt in bytes
+        mask (string): the mask to use (can also contain hashcat options)
+    """
     s_hash = h.hex() + ":" + s.hex()
     c = "hashcat-5.1.0/hashcat64.bin -m 20 -a 3 --hex-salt {} {}".format(s_hash, mask)
     os.system(c)
-    os.system(c + " --show")
 
 
 if __name__ == "__main__":
-    input_file = "bgp_simulation.pcap"
-    hashes = parse_packets_md5(input_file)
-    # password = "test"
-    # check_password(hashes[0][0],hashes[0][1],password)
-    mask = "-1 ?u?l -2 ?u?l?d ?2?2?2?2?2?2 --increment --increment-min 4"
+    input_file = "bgp_crafted.pcap"
+    hashes = parse_signed_packets_md5(input_file)
+    check_password(hashes[0][0], hashes[0][1], "nana")
+    mask = "-1 ?u?l -2 ?u?l?d ?1?1?1?1?1?1 --increment --increment-min 4 --potfile-disable"
     launch_hashcat(hashes[0][0], hashes[0][1], mask)
